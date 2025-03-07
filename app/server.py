65468e588ac09c2a2d1418c5fddd6e50242e21fe
Jahez Abraham Johny <jaj@sureflow.com>
2025-03-07 13:05:29 +0400

    update

--- app/server.py
+++ app/server.py
@@ -2,30 +2,29 @@ from fastapi import FastAPI
 import joblib
 import numpy as np
 
-model = joblib.load('model.joblib')
+model = joblib.load('app/model.joblib')
 
 class_names = np.array(['setosa', 'versicolor', 'virginica'])
 
 app = FastAPI()
 
 @app.get('/')
-def reed_root():
-    return {'message':'Iris model API'}
+def read_root():
+    return {'message': 'Iris model API'}
 
-@app.get('/predict')
+@app.post('/predict')
 def predict(data: dict):
     """
-    Predict the class of a give set of features. 
+    Predicts the class of a given set of features.
 
     Args:
         data (dict): A dictionary containing the features to predict.
-        eg. {"features": [5.1, 3.5, 1.4, 0.2]}
+        e.g. {"features": [1, 2, 3, 4]}
 
     Returns:
         dict: A dictionary containing the predicted class.
-    """
-    features = np.array(data['features'].reshape(1, -1))
+    """        
+    features = np.array(data['features']).reshape(1, -1)
     prediction = model.predict(features)
-    class_name = class_names[prediction][0] 
+    class_name = class_names[prediction][0]
     return {'predicted_class': class_name}
- 
